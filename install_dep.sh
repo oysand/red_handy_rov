a4889da039adea24ca2e2c374cb06bbc05bbd9b7
oysand <oystein.utbjoe.andersland@gmail.com>
2021-04-01 20:16:39 +0200

    Install dependencies

---
+++ install_dep.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+### Install ros melodic
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+sudo apt update &&\
+    apt install -y \
+    ros-melodic-desktop-full
+
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source /opt/ros/melodic/setup.bash
+
+sudo apt install -y \
+    python-rosdep \
+    python-rosinstall \
+    python-rosinstall-generator \
+    python-wstool \
+    build-essential \
+    python-rosdep \
+    python-catkin-tools \
+    ros-melodic-uuv-simulator
+
+sudo rosdep init
+rosdep update
+
+cd ~/catkin_ws
+rosdep install --from-paths ./src --ignore-packages-from-source --rosdistro melodic -y
+
+catkin build
+
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/catkin_ws/devel/setup.bash
